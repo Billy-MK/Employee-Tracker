448db1fbcff156434169c82f40e2a90f12bb4e71
Billy Keating <wmontoothkeating@gmail.com>
2021-03-17 14:15:36 -0700

    Created main menu inquirer questions with switch statement directing to functions to be created

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const mysql = require('mysql');
 const inquirer = require('inquirer');
 const cTable = require('console.table');
+const { allowedNodeEnvironmentFlags } = require('node:process');
 const dotenv = require('dotenv').config()
 
 // create the connection information for the sql database
@@ -17,3 +18,41 @@ const connection = mysql.createConnection({
     password: process.env.DB_PASS,
     database: 'employees_db',
   });
+
+const main = () => {
+    inquirer.prompt(
+        {
+            name: 'mainMenu',
+            type: 'list',
+            message: 'What would you like to do?',
+            choices: ['View Departments', 'View Roles', 'View Employees', 'Add Departments', 'Add Roles', 'Add Employees', 'Update Employee Roles'],
+        }
+    ).then((answer) => {
+        switch(answer.mainMenu) {
+            case 'View Departments': 
+                viewDepartments();
+                break;
+            case 'View Roles': 
+                viewRoles();
+                break;
+            case 'View Employees': 
+                viewEmployees();
+                break;
+            case 'Add Departments': 
+                addDepartments();
+                break;
+            case 'Add Roles': 
+                addRoles();
+                break;
+            case 'Add Employees':
+                addEmployees();
+                break;
+            case 'Update Employee Roles':
+                updateEmployeeRoles();
+                break;
+            default:
+                console.log(`Invalid action: ${answer.action}`);
+                break;
+        }
+    })
+}
