a086749877a03c5934ff7e7d49c1d6897ebca5e7
Billy Keating <wmontoothkeating@gmail.com>
2021-03-17 14:00:14 -0700

    Added connection setup and some dependencies

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
 const mysql = require('mysql');
 const inquirer = require('inquirer');
 const cTable = require('console.table');
+const dotenv = require('dotenv').config()
+
+// create the connection information for the sql database
+const connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+  
+    // port
+    port: 3306,
+  
+    // username
+    user: process.env.DB_USER,
+  
+    // password
+    password: process.env.DB_PASS,
+    database: 'employees_db',
+  });
